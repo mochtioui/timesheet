c32e6b97b01c9138bbd51c7955372845446e4753
mochtioui <49149242+mochtioui@users.noreply.github.com>
2021-10-15 14:55:03 +0100

    Update EntrepriseServiceImpl.java

--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -1,86 +1,60 @@
-package tn.esprit.spring.services;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import tn.esprit.spring.entities.Departement;
-import tn.esprit.spring.entities.Entreprise;
-import tn.esprit.spring.repository.DepartementRepository;
-import tn.esprit.spring.repository.EntrepriseRepository;
-
-@Service
-public class EntrepriseServiceImpl implements IEntrepriseService {
-private static final Logger l = LogManager.getLogger(EntrepriseServiceImpl.class);
-	@Autowired
-    EntrepriseRepository entrepriseRepoistory;
-	@Autowired
-	DepartementRepository deptRepoistory;
-	
-	public int ajouterEntreprise(Entreprise entreprise) {
-		entrepriseRepoistory.save(entreprise);		
-l.info("Entreprise Ajouté");
-		return entreprise.getId();
+	@@ -31,42 +32,52 @@ public int ajouterDepartement(Departement dep) {
 	}
 
-	public int ajouterDepartement(Departement dep) {
-		deptRepoistory.save(dep);
-l.info("Departement Ajouté");
-		return dep.getId();
-	}
-	
 	public void affecterDepartementAEntreprise(int depId, int entrepriseId) {
-		//Le bout Master de cette relation N:1 est departement  
-				//donc il faut rajouter l'entreprise a departement 
-				// ==> c'est l'objet departement(le master) qui va mettre a jour l'association
-				//Rappel : la classe qui contient mappedBy represente le bout Slave
-				//Rappel : Dans une relation oneToMany le mappedBy doit etre du cote one.
-				Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
-				Departement depManagedEntity = deptRepoistory.findById(depId).get();
-				
-				depManagedEntity.setEntreprise(entrepriseManagedEntity);
-				deptRepoistory.save(depManagedEntity);
-l.info("vous avez  affecter un Departement a une Entreprise ");
-		
+				Optional <Entreprise> entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId);
+				Optional <Departement> depManagedEntity = deptRepoistory.findById(depId);
+				if(entrepriseManagedEntity.isPresent() && depManagedEntity.isPresent()) {
+				depManagedEntity.get().setEntreprise(entrepriseManagedEntity.get());
+				deptRepoistory.save(depManagedEntity.get());
+				}
 	}
-	
+
 	public List<String> getAllDepartementsNamesByEntreprise(int entrepriseId) {
-		Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
+		Optional <Entreprise> entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId);
 		List<String> depNames = new ArrayList<>();
-		for(Departement dep : entrepriseManagedEntity.getDepartements()){
+		if(entrepriseManagedEntity.isPresent()) {
+		for(Departement dep : entrepriseManagedEntity.get().getDepartements()){
 			depNames.add(dep.getName());
-
-
-l.info("getAllDepartementsNamesByEntreprise ");
 		}
-		
+
 		return depNames;
+		}
+		return null;
 	}
 
 	@Transactional
 	public void deleteEntrepriseById(int entrepriseId) {
-		entrepriseRepoistory.delete(entrepriseRepoistory.findById(entrepriseId).get());	
+		Optional <Entreprise> entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId);
+		if(entrepriseManagedEntity.isPresent()) {
+			entrepriseRepoistory.delete(entrepriseManagedEntity.get());	
+		}
 
-l.warn("Vous avez supprimer une entreprise  ");
 	}
 
 	@Transactional
 	public void deleteDepartementById(int depId) {
-		deptRepoistory.delete(deptRepoistory.findById(depId).get());	
+		Optional <Departement> depManagedEntity = deptRepoistory.findById(depId);
+		if(depManagedEntity.isPresent()) {
+			deptRepoistory.delete(depManagedEntity.get());	
+		}
 
-
-l.info("Vous avez supprimer un Departement");
 	}
 
 
 	public Entreprise getEntrepriseById(int entrepriseId) {
-		return entrepriseRepoistory.findById(entrepriseId).get();
-
+		Optional <Entreprise> entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId);
+		if(entrepriseManagedEntity.isPresent()) {
+			return entrepriseManagedEntity.get();	
+		}
+		return null;
 	}
 
 }
