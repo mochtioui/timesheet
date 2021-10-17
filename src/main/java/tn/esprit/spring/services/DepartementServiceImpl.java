9fff0b1a24993def83b81999a5fd86c6a44c0cef
ayoub0111 <47676062+ayoub0111@users.noreply.github.com>
2021-10-17 11:31:59 +0200

    Corriger les erreur de Class Departement

--- src/main/java/tn/esprit/spring/services/DepartementServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/DepartementServiceImpl.java
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Service;
 
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.repository.DepartementRepository;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 @Service
 public class DepartementServiceImpl implements IDepartementService {
 
@@ -15,8 +16,12 @@ public class DepartementServiceImpl implements IDepartementService {
 	@Autowired
 	DepartementRepository deptRepoistory;
 
+	private static final Logger log = LogManager.getLogger(DepartementServiceImpl.class);
 
 	public List<Departement> getAllDepartements() {
+		
+		List<Departement> dep = (List<Departement>) deptRepoistory.findAll();
+		log.info(dep);
 		return (List<Departement>) deptRepoistory.findAll();
 	}
 
