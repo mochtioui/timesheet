b9a99a8911b373e80ed0fc71ab1ebab098cb9a55
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-10-18 01:21:56 +0000

    adding the necessary imports to the test class

--- src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
+++ src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
@@ -8,10 +8,19 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.services.EmployeServiceImpl;;
+
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class TimesheetApplicationTests {
 
+	
+
+	
 	@Test
 	void contextLoads() {
 	}
