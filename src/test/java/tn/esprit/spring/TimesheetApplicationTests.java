8a215ce3b64db3a51786f348b2c90ad267fdcab5
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-10-18 01:16:07 +0000

    adding the Junit dependency

--- src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
+++ src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 class TimesheetApplicationTests {
 
 	@Test
