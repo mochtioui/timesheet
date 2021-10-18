36e2643f5648f2b6152bd7f937939b2d92f271c3
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-10-18 01:19:29 +0000

    updating the pom file + adding the required imports for the Junit test

--- src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
+++ src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
@@ -1,7 +1,13 @@
 package tn.esprit.spring;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 class TimesheetApplicationTests {
