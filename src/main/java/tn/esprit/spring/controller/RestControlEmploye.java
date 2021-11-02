022b2aac67790cc9b4d2d76aa4675f91c9002f65
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-11-02 19:48:49 +0000

    changing the application.properties file

--- src/main/java/tn/esprit/spring/controller/RestControlEmploye.java
+++ src/main/java/tn/esprit/spring/controller/RestControlEmploye.java
@@ -159,7 +159,6 @@ public class RestControlEmploye {
 	}
 
 	
-	//TODO
 	public List<Timesheet> getTimesheetsByMissionAndDate(Employe employe, Mission mission, Date dateDebut,
 			Date dateFin) {
 		return iemployeservice.getTimesheetsByMissionAndDate(employe, mission, dateDebut, dateFin);
