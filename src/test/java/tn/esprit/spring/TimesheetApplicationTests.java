5fe4fc53fa9258e61a9b898ee7ac469000b48778
unknown <brahim.hmida1@esprit.tn>
2021-11-04 20:59:49 +0100

    add test

--- src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
+++ src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
@@ -1,10 +1,20 @@
 package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.spring.repository.ContratRepository;
+import tn.esprit.spring.repository.EmployeRepository;
 
 @SpringBootTest
 class TimesheetApplicationTests {
+	
+	@Autowired
+	ContratRepository contratRepository;
+	
+	@Autowired
+	EmployeRepository employeRepository;
 
 	@Test
 	void contextLoads() {
