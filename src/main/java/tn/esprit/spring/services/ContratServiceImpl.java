2fb65d21bfae404b07da26b4d3913425c8da0e21
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-11-07 18:41:04 +0000

    adding a contract , using the Junit testing methods

--- src/main/java/tn/esprit/spring/services/ContratServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/ContratServiceImpl.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import tn.esprit.spring.entities.Contrat;
+import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.repository.ContratRepository;
+import tn.esprit.spring.repository.EmployeRepository;
 
 @Service
 public class ContratServiceImpl implements IContratService {
@@ -14,10 +16,13 @@ public class ContratServiceImpl implements IContratService {
 
 	@Autowired
 	ContratRepository contratRepository;
+	@Autowired
+	EmployeRepository empRepo;
 
 
 	public List<Contrat> getAllContrats() {
 		return (List<Contrat>) contratRepository.findAll();
 	}
+	
 
 }
--- src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
+++ src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
@@ -6,15 +6,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Entreprise;
 import tn.esprit.spring.entities.Role;
+import tn.esprit.spring.services.ContratServiceImpl;
+import tn.esprit.spring.services.EmployeServiceImpl;
 import tn.esprit.spring.services.IEmployeService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import static org.assertj.core.api.Assertions.assertThat;
+
+
+import tn.esprit.spring.entities.Contrat;
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.services.IEntrepriseService;
 @RunWith(SpringRunner.class)
@@ -22,11 +29,20 @@ import tn.esprit.spring.services.IEntrepriseService;
 public class TimesheetApplicationTests    {
 
 	private static final Logger l = LogManager.getLogger(TimesheetApplicationTests.class);
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
 	@Autowired
 
       IEmployeService  empService ;
 	@Autowired
+
+    EmployeServiceImpl  empcontract ;
+	@Autowired
 	IEntrepriseService ientrepriseservice;
+	
+	@Autowired
+	ContratServiceImpl contratService;
+	
 	
 	@Test
 	public void testAjoutEmploye() {
@@ -44,6 +60,7 @@ public class TimesheetApplicationTests    {
 
        
 	    }
+	
 	
 	
 	/*@Test
@@ -86,13 +103,35 @@ public class TimesheetApplicationTests    {
 
 	}
 	
-	@Test
+	/*@Test
 	public void testDesaffectEmployeeToADepartement()
 	{
 		empService.desaffecterEmployeDuDepartement(14, 1);
 		l.info("you have affected  an employee to a new departement  !");
 
+	}*/
+	
+	@Test
+	public void testAjoutContrat() {
+	String date ="31/12/2021";
+     Date date1 = null;
+	try {
+		date1 = simpleDateFormat.parse(date);
+	} catch (ParseException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
 	}
+
+		Contrat e = new Contrat( date1 ,"CDI", 6000);
+		empcontract.ajouterContrat(e);
+		
+		assertThat(e.getTypeContrat()).isEqualTo("CDI");
+	assertThat(e.getSalaire()).isGreaterThan(3000);
+
+
+
+       
+	    }
 	
 
 }
