1c6800af0c6ac40cd7400064b3e85b927d859942
ayoub0111 <47676062+ayoub0111@users.noreply.github.com>
2021-11-08 12:26:04 +0100

    last

--- src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
+++ src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
@@ -63,10 +63,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import tn.esprit.spring.entities.Contrat;
 import tn.esprit.spring.services.ContratServiceImpl;
-@TestMethodOrder(OrderAnnotation.class)
+
+
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TimesheetApplicationTests  extends AbstractTest   {
+public class TimesheetApplicationTests extends AbstractTest {
 
 	private static final Logger logger= LogManager.getLogger(TimesheetApplicationTests.class);
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
@@ -84,222 +86,12 @@ public class TimesheetApplicationTests  extends AbstractTest   {
 
 	@Autowired
 	ContratServiceImpl ser ; 
-	
 
 	@Override
 	   @Before
 	   public void setUp() {
 	      super.setUp();
    }
-
-
-	
-	
-	@Test
-	@Order(1)
-	public void addContrat() throws ParseException {
-			
-		java.util.Date date=new java.util.Date();
-		java.sql.Date sqlDate=new java.sql.Date(date.getTime());
-		Contrat contrat = new Contrat(sqlDate ,"test",12);
-		Contrat Contratadded = ser.addContrat(contrat);
-		Assert.assertEquals(contrat.getTypeContrat(), Contratadded.getTypeContrat());
-	}
-	
-	@Test
-	@Order(3)
-	public void getAllContrat() {
-		List<Contrat> listcontrats = ser.getAllContrats();
-		Assert.assertEquals(listcontrats.size(), listcontrats.size());
-	}
-	
-	@Test
-	@Order(2)
-	public void updateContrat() throws ParseException   {
-		java.util.Date date=new java.util.Date();
-		java.sql.Date sqlDate=new java.sql.Date(date.getTime());
-		Contrat con = new Contrat(2,sqlDate ,"test",15);
-		Contrat contratModified = ser.updateContrat(con);
-		Assert.assertEquals(con.getTypeContrat(), contratModified.getTypeContrat());
-	}
-	
-	@Test
-	@AfterAll
-	public void deleteContrat() {
-		ser.remove("1");
-		Assert.assertNull(ser.getContrat("1"));
-		
-		
-	}
-
-
-   /*** chtioui */
-   @Test
-	   public void ajouterEntreprise() throws Exception {
-	      String uri = "/ajouterEntreprise";
-	      Entreprise entreprise = new Entreprise();
-	      entreprise.setId(2);;
-	      entreprise.setName("Chtioui");
-	      entreprise.setRaisonSocial("Nice");
-	      String inputJson = super.mapToJson(entreprise);
-	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
-	         .contentType(MediaType.APPLICATION_JSON_VALUE)
-	         .content(inputJson)).andReturn();
-	      
-	      int status = mvcResult.getResponse().getStatus();
-	      assertEquals(200, status);
-	      if(status == 200) {
-	    	  logger.info("l'entreprise a été ajouté avec succés");
-	      }else if(status == 201) {
-	    	  logger.info("l'entreprise a été ajouté avec succés");
-	      }else if(status >= 400 && status <500) {
-	    	  logger.fatal("erreur coté client :  code reponse :  "+status);
-	      }else if(status >= 500){
-	    	  logger.error("erreur coté serveur :  code reponse :  "+status);
-	      }
-	     
-	   }
-
-
-
-	    @Test
-	   public void ajouterDepartement() throws Exception {
-	      String uri = "/ajouterDepartement";
-	      Departement departement = new Departement();
-	      departement.setId(1);;
-	      departement.setName("Info");
-	 
-	      String inputJson = super.mapToJson(departement);
-	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
-	         .contentType(MediaType.APPLICATION_JSON_VALUE)
-	         .content(inputJson)).andReturn();
-	      
-	      int status = mvcResult.getResponse().getStatus();
-	      assertEquals(200, status);
-	      if(status == 200) {
-	    	  logger.info("le departement a été ajouté avec succés");
-		      }else if(status >= 400 && status <500) {
-		    	  logger.fatal("erreur coté client :  code reponse :  "+status);
-		      }else if(status >= 500){
-		    	  logger.error("erreur coté serveur :  code reponse :  "+status);
-		      }
-	   }
-
-
-
-	   @Test
-	   public void getEntrepriseById() throws Exception {
-	      String uri = "/getEntrepriseById/1";
-	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
-	         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
-	      
-	      int status = mvcResult.getResponse().getStatus();
-	      assertEquals(200, status);
-	      String content = mvcResult.getResponse().getContentAsString();
-	      Entreprise entreprise = super.mapFromJson(content, Entreprise.class);
-	      assertTrue(entreprise != null);
-	          if(status == 200) {
-	        	  logger.info("l'entreprise a été recupérée avec succés");
-		      }else if(status >= 400 && status <500) {
-		    	  logger.fatal("erreur coté client :  code reponse :  "+status);
-		      }else if(status >= 500){
-		    	  logger.error("erreur coté serveur :  code reponse :  "+status);
-		      }
-	   }
-
-
-	   @Test
-	   public void getAllDepartementsNamesByEntreprise() throws Exception {
-	      String uri = "/getAllDepartementsNamesByEntreprise/1";
-	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
-	         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
-	      
-	      int status = mvcResult.getResponse().getStatus();
-	      assertEquals(200, status);
-	     
-	      if(status == 200) {
-	    	  logger.info("les deârtmenets de l'entreprise ont été recupérées avec succés");
-	      }else if(status >= 400 && status <500) {
-	    	  logger.fatal("erreur coté client :  code reponse :  "+status);
-	      }else if(status >= 500){
-	    	  logger.error("erreur coté serveur :  code reponse :  "+status);
-	      }
-	   }
-	
-	
-	   @Test
-	   public void affecterDepartementAEntreprise() throws Exception {
-	      String uri = "/affecterDepartementAEntreprise/6/10";
-	   
-	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(uri)
-	         .contentType(MediaType.APPLICATION_JSON_VALUE)
-	        ).andReturn();
-	      
-	      int status = mvcResult.getResponse().getStatus();
-	      assertEquals(200, status);
-	      if(status == 200) {
-	    	  logger.info("le département a été affecté à l'entreprise avec succés");
-	      }else if(status >= 400 && status <500) {
-	    	  logger.fatal("erreur coté client :  code reponse :  "+status);
-	      }else if(status >= 500){
-	    	  logger.error("erreur coté serveur :  code reponse :  "+status);
-	      }
-	   }
-	  
- 
-
- @Test
-	   public void deleteEntreprise() throws Exception {
-	      String uri = "/deleteEntrepriseById/13";
-	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
-	      int status = mvcResult.getResponse().getStatus();
-	      assertEquals(200, status);
-	      if(status == 200) {
-	    	  logger.info("l'entreprise a été supprimée avec succés");
-	      }else if(status >= 400 && status <500) {
-	    	  logger.fatal("erreur coté client :  code reponse :  "+status);
-	      }else if(status >= 500){
-	    	  logger.error("erreur coté serveur :  code reponse :  "+status);
-	      }
-	   }
-	   
-	   
-	   @Test
-	   public void deleteDepartement() throws Exception {
-	      String uri = "/deleteDepartementById/9";
-	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
-	      int status = mvcResult.getResponse().getStatus();
-	      assertEquals(200, status);
-	      if(status == 200) {
-	    	  logger.info("le département a été recupérée avec succés");
-	      }else if(status >= 400 && status <500) {
-	    	  logger.fatal("erreur coté client :  code reponse :  "+status);
-	      }else if(status >= 500){
-	    	  logger.error("erreur coté serveur :  code reponse :  "+status);
-	      }
-	   }
-	   
-	   
-	   
-
-	   /****chtioui end */
-	
-	@Test
-	public void testAjoutEmploye() {
-		
-		Employe e = new Employe(false ,"khaoula.khmiri@esprit.tn", "test" ,"test" ,"test" , Role.CHEF_DEPARTEMENT);
-		empService.addOrUpdateEmploye(e);
-		logger.info("you have added " +e.getNom() +" as a new employee !");
-		logger.info("the employee you just added had  " +e.getId() +" as an ID  !");
-		assertThat(e.getNom()).isEqualTo("test");
-		assertThat(e.getPassword()).isEqualTo("test");
-	assertThat(e.getId()).isGreaterThan(0);
-
- }
-	
-	
-	
-	
 
 	@Test
 	public void testAjoutEntrepriseDep() {
@@ -329,11 +121,18 @@ public class TimesheetApplicationTests  extends AbstractTest   {
 			ientrepriseservice.affecterDepartementAEntreprise(2, 1);
 			logger.info("Sortie de la méthode");
 		}
-	
-	
-	
-	
-	
+	@Test
+	public void testAjoutEmploye() {
+		
+		Employe e = new Employe(false ,"khaoula.khmiri@esprit.tn", "test" ,"test" ,"test" , Role.CHEF_DEPARTEMENT);
+		empService.addOrUpdateEmploye(e);
+		logger.info("you have added " +e.getNom() +" as a new employee !");
+		logger.info("the employee you just added had  " +e.getId() +" as an ID  !");
+		assertThat(e.getNom()).isEqualTo("test");
+		assertThat(e.getPassword()).isEqualTo("test");
+	    assertThat(e.getId()).isGreaterThan(0);
+
+ }
 	@Test
 	public void testDeleteEmployee() {
 	     empService.deleteEmployeById(13);
@@ -353,7 +152,6 @@ public class TimesheetApplicationTests  extends AbstractTest   {
 	{
 		empService.affecterEmployeADepartement(14, 1);
 		logger.info("you have affected  an employee to a new departement  !");
-
 	}
 	
 	@Test
@@ -363,7 +161,6 @@ public class TimesheetApplicationTests  extends AbstractTest   {
 		logger.info("you have affected  an employee to a new departement  !");
 
 	}
-	
 	@Test
 	public void testAjoutContrat() {
 	String date ="31/12/2021";
@@ -390,6 +187,164 @@ public class TimesheetApplicationTests  extends AbstractTest   {
 		
 		logger.debug("you have affected an employee to a contract ");
   }
-	
-
+	  @Test
+	   public void ajouterEntreprise() throws Exception {
+	      String uri = "/ajouterEntreprise";
+	      Entreprise entreprise = new Entreprise();
+	      entreprise.setId(7);;
+	      entreprise.setName("Chtioui");
+	      entreprise.setRaisonSocial("Nice");
+	      String inputJson = super.mapToJson(entreprise);
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+	         .contentType(MediaType.APPLICATION_JSON_VALUE)
+	         .content(inputJson)).andReturn();
+	      
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+	      if(status == 200) {
+	    	  logger.info("l'entreprise a été ajouté avec succés");
+	      }else if(status == 201) {
+	    	  logger.info("l'entreprise a été ajouté avec succés");
+	      }else if(status >= 400 && status <500) {
+	    	  logger.fatal("erreur coté client :  code reponse :  "+status);
+	      }else if(status >= 500){
+	    	  logger.error("erreur coté serveur :  code reponse :  "+status);
+	      }
+	     
+	   }
+	  @Test
+	   public void ajouterDepartement() throws Exception {
+	      String uri = "/ajouterDepartement";
+	      Departement departement = new Departement();
+	      departement.setId(8);;
+	      departement.setName("Info");
+	 
+	      String inputJson = super.mapToJson(departement);
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+	         .contentType(MediaType.APPLICATION_JSON_VALUE)
+	         .content(inputJson)).andReturn();
+	      
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+	      if(status == 200) {
+	    	  logger.info("le departement a été ajouté avec succés");
+		      }else if(status >= 400 && status <500) {
+		    	  logger.fatal("erreur coté client :  code reponse :  "+status);
+		      }else if(status >= 500){
+		    	  logger.error("erreur coté serveur :  code reponse :  "+status);
+		      }
+	   }
+	   @Test
+	   public void getEntrepriseById() throws Exception {
+	      String uri = "/getEntrepriseById/2";
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+	         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+	      
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+	      String content = mvcResult.getResponse().getContentAsString();
+	      Entreprise entreprise = super.mapFromJson(content, Entreprise.class);
+	      assertTrue(entreprise != null);
+	          if(status == 200) {
+	        	  logger.info("l'entreprise a été recupérée avec succés");
+		      }else if(status >= 400 && status <500) {
+		    	  logger.fatal("erreur coté client :  code reponse :  "+status);
+		      }else if(status >= 500){
+		    	  logger.error("erreur coté serveur :  code reponse :  "+status);
+		      }
+	   }
+	   @Test
+	   public void getAllDepartementsNamesByEntreprise() throws Exception {
+	      String uri = "/getAllDepartementsNamesByEntreprise/1";
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+	         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+	      
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+	     
+	      if(status == 200) {
+	    	  logger.info("les deârtmenets de l'entreprise ont été recupérées avec succés");
+	      }else if(status >= 400 && status <500) {
+	    	  logger.fatal("erreur coté client :  code reponse :  "+status);
+	      }else if(status >= 500){
+	    	  logger.error("erreur coté serveur :  code reponse :  "+status);
+	      }
+	   }
+	   @Test
+	   public void affecterDepartementAEntreprise() throws Exception {
+	      String uri = "/affecterDepartementAEntreprise/6/10";
+	   
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(uri)
+	         .contentType(MediaType.APPLICATION_JSON_VALUE)
+	        ).andReturn();
+	      
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+	      if(status == 200) {
+	    	  logger.info("le département a été affecté à l'entreprise avec succés");
+	      }else if(status >= 400 && status <500) {
+	    	  logger.fatal("erreur coté client :  code reponse :  "+status);
+	      }else if(status >= 500){
+	    	  logger.error("erreur coté serveur :  code reponse :  "+status);
+	      }
+	   }
+	  
+	   @Test
+	   public void deleteEntreprise() throws Exception {
+	      String uri = "/deleteEntrepriseById/8";
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+	      if(status == 200) {
+	    	  logger.info("l'entreprise a été supprimée avec succés");
+	      }else if(status >= 400 && status <500) {
+	    	  logger.fatal("erreur coté client :  code reponse :  "+status);
+	      }else if(status >= 500){
+	    	  logger.error("erreur coté serveur :  code reponse :  "+status);
+	      }
+	   }
+	   @Test
+	   public void deleteDepartement() throws Exception {
+	      String uri = "/deleteDepartementById/9";
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+	      if(status == 200) {
+	    	  logger.info("le département a été recupérée avec succés");
+	      }else if(status >= 400 && status <500) {
+	    	  logger.fatal("erreur coté client :  code reponse :  "+status);
+	      }else if(status >= 500){
+	    	  logger.error("erreur coté serveur :  code reponse :  "+status);
+	      }
+	   }
+		@Test
+		public void addContrat() throws ParseException {
+				
+			java.util.Date date=new java.util.Date();
+			java.sql.Date sqlDate=new java.sql.Date(date.getTime());
+			Contrat contrat = new Contrat(sqlDate ,"test",12);
+			Contrat Contratadded = ser.addContrat(contrat);
+			Assert.assertEquals(contrat.getTypeContrat(), Contratadded.getTypeContrat());
+		}
+		@Test
+		public void getAllContrat() {
+			List<Contrat> listcontrats = ser.getAllContrats();
+			Assert.assertEquals(listcontrats.size(), listcontrats.size());
+		}
+		@Test
+		public void updateContrat() throws ParseException   {
+			java.util.Date date=new java.util.Date();
+			java.sql.Date sqlDate=new java.sql.Date(date.getTime());
+			Contrat con = new Contrat(2,sqlDate ,"test",15);
+			Contrat contratModified = ser.updateContrat(con);
+			Assert.assertEquals(con.getTypeContrat(), contratModified.getTypeContrat());
+		}
+		@Test
+		@AfterAll
+		public void deleteContrat() {
+			ser.remove("1");
+			Assert.assertNull(ser.getContrat("1"));
+			
+			
+		}
 }
